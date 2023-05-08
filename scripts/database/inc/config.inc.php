f065600d4889fc4f0672dd226d2452adad9232c4
vladoniscenko <vladoniscenko@outlook.com>
2023-05-08 12:56:29 +0200

    first commit

---
+++ scripts/database/inc/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbhost = "localhost";
+    $dbuser = "";
+    $dbpass = "";
+    $db = "";
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+  
+?>
